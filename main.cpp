abc8dac2801914abf11b01fcc54cd97c82af37c6
Kevin Chalmers <kevin.chalmers@roehampton.ac.uk>
2022-01-24 21:02:24 +0000

    Added necessary enums

--- main.cpp
+++ main.cpp
@@ -3,6 +3,26 @@
 using std::cout;
 using std::endl;
 
+enum BASE
+{
+    BINARY,
+    OCTAL,
+    DECIMAL,
+    HEXADECIMAL,
+    ROMAN
+};
+
+enum ROMAN
+{
+    I = 1,
+    V = 5,
+    X = 10,
+    L = 50,
+    C = 100,
+    D = 500,
+    M = 1000
+};
+
 int main(int argc, char **argv)
 {
     for (int i = 1; i < argc; ++i)
